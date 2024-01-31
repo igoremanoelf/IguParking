00158a342e494443e120d259042a8f305477cc4a
tarde.cetafaju <tarde.cetafaju@PC03LAB2536>
2024-01-31 16:32:00 -0300

    Adicionar arquivos de projeto.

---
+++ IguParking/Estacionamento.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IguParking
+{
+    internal class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento( decimal precoInicial , decimal precoHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo:");
+            string placa = Console.ReadLine();
+
+            if (veiculos.Any(x => x.Equals(placa.ToUpper())))
+            {
+                Console.WriteLine("Já existe um veículo com essa placa.");
+            } else
+            {
+                veiculos.Add(placa);
+                Console.WriteLine("Veículo cadastrado com sucesso.");
+
+            }
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Informe a placa do  veículo a ser removido:");
+            string placa = Console.ReadLine();
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas do veículo no pátio:");
+                int horas = Convert.ToInt32(Console.ReadLine());
+                decimal precoTotal = precoInicial + precoHora * horas;
+                veiculos.Remove(placa);
+                Console.WriteLine($" O veículo de placa {placa} foi removido com sucesso. Deverá ser pago {precoTotal} reais");
+            }
+             else
+            {
+                Console.WriteLine($"O veículo da placa {placa} não foi encontrado");
+            }
+        }
+
+        public void ListarVeiculo()
+        {
+            if (veiculos.Any())
+            {
+                Console.Write("Os veículos estacionados são:");
+                foreach (var v in veiculos)
+                {
+                    Console.WriteLine(v);
+                }
+
+            } else
+            {
+                Console.WriteLine("Não há veículos no pátio.");
+            }
+            }
+        }
+    }
+
---
+++ IguParking/Program.cs
@@ -0,0 +1,59 @@
+﻿// Declaração de variáveis
+
+using IguParking;
+
+decimal precoInicial = 0;
+decimal precoHora = 0;
+
+Console.WriteLine("Sejam bem vindos ao IguParking");
+Console.WriteLine("-------------------------------");
+
+Console.WriteLine("Informe o preço inicial");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o preço por hora no estacionamento");
+precoHora = Convert.ToDecimal(Console.ReadLine());
+
+
+//Instancia a classe Estacionamento
+Estacionamento es = new Estacionamento(precoInicial, precoHora);
+
+//Loop
+
+bool menu = true;
+
+while(menu)
+
+{
+    Console.Clear();
+    Console.WriteLine("Escolha uma opção:");
+    Console.WriteLine("1 - Adicionar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículo");
+    Console.WriteLine("4 - Sair do sistema");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.AdicionarVeiculo();
+            break;
+        case "2":
+            es.RemoverVeiculo();
+            break;
+        case "3":
+            es.ListarVeiculo();
+            break;
+        case "4":
+            menu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inexistente. Digite uma opção válida.");
+            break;
+    }
+
+    Console.WriteLine("Digite uma tecla para continuar");
+    Console.ReadLine();
+
+
+}
